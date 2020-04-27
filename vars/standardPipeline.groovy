6c88c15532291fa79353785d3c5f13914f6e9eb5
Nimish Mehta <nimish.mehta@gmail.com>
2020-04-27 15:10:37 +0530

    modified jenkinsfile

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -1,38 +1,7 @@
- def call(body) {
+def info(message) {
+    echo "INFO: ${message}"
+}
 
-        def config = [:]
-        body.resolveStrategy = Closure.DELEGATE_FIRST
-        body.delegate = config
-        body()
-
-        node {
-            // Clean workspace before doing anything
-            deleteDir()
-
-            try {
-                stage ('Clone') {
-                    checkout scm
-                }
-                stage ('Build') {
-                    sh "echo 'building ${config.projectName} ...'"
-                }
-                stage ('Tests') {
-                    parallel 'static': {
-                        sh "echo 'shell scripts to run static tests...'"
-                    },
-                    'unit': {
-                        sh "echo 'shell scripts to run unit tests...'"
-                    },
-                    'integration': {
-                        sh "echo 'shell scripts to run integration tests...'"
-                    }
-                }
-                stage ('Deploy') {
-                    sh "echo 'deploying to server ${config.serverDomain}...'"
-                }
-            } catch (err) {
-                currentBuild.result = 'FAILED'
-                throw err
-            }
-        }
-    }
+def warning(message){
+    echo "WARNING: ${message}"
+}
